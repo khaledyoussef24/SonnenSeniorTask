54924c27ecab90d2f5fe1046955821bea7c5ef17
khaledyoussef24 <mahmoudk@incubaid.com>
2025-07-03 00:55:41 +0300

    fixing spilling mistake

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "kubernetes_namespace" "app" {
 resource "helm_release" "nginx" {
   name       = var.release_name
   namespace  = kubernetes_namespace.app.metadata[0].name
-  chart      = "../charts/nginx"
+  chart      = "../Charts/nginx"
   values     = [file("${path.module}/helm_values.yaml")]
   timeout    = 300
   atomic     = true
